d5815d6fb80b5bb9ffc949bdcaca20c4483997eb
Valeria del Rio <valeriadlrio@gmail.com>
2021-01-14 11:53:33 -0300

    Primer commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+// Express hbs
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    res.render('home', { nombre: 'vAleria' });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+app.listen(port, () => {
+    console.log(`escuchando peticiones en el puerto ${port}`);
+})
